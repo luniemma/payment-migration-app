da6014cfe13e59ea90a31d23052a5fdc6715ff20
luniemma12 <luniemma08@gmail.com>
2025-05-25 16:24:37 -0500

    refactor: remove duplicate client_id assignment in Terraform provider configuration

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -10,7 +10,7 @@ terraform {
 provider "azurerm" {
   features {}
 
+  client_id       = var.client_id
   tenant_id       = var.tenant_id
   subscription_id = var.subscription_id
-  client_id       = var.client_id
 }
